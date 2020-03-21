32bacb1bc04d759ef7f75d3ae739ea86b23d23d7
Super Oleg <oleg_in@bk.ru>
2020-03-21 16:54:32 +0300

    PurchaseList added

--- 10_SQLAndHibernate/SQL 10.2.3/src/main/java/Main.java
+++ 10_SQLAndHibernate/SQL 10.2.3/src/main/java/Main.java
@@ -1,7 +1,4 @@
-import model.Courses;
-import model.Students;
-import model.Subscriptions;
-import model.Teachers;
+import model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -23,13 +20,17 @@ public class Main {
 
             Subscriptions subscription = session.get(Subscriptions.class, new Subscriptions().new SubscriptionKey(course,course.getStudents().get(0)));
 
-            System.out.println(subscription);
+            PurchaseList purschaseList = session.get(PurchaseList.class, new PurchaseList().new PurchaseListKey(course.getName(),course.getStudents().get(0).getName()));
 
-            System.out.println(course);
+            System.out.println(purschaseList);
 
-            System.out.println(student);
-
-            System.out.println(teacher);
+//            System.out.println(subscription);
+//
+//            System.out.println(course);
+//
+//            System.out.println(student);
+//
+//            System.out.println(teacher);
         }
     }
 }
---
+++ 10_SQLAndHibernate/SQL 10.2.3/src/main/java/model/PurchaseList.java
@@ -0,0 +1,42 @@
+package model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@IdClass(PurchaseList.PurchaseListKey.class)
+public class PurchaseList {
+    @Id
+    @Column(name = "course_name")
+    private String courseName;
+
+    private int price;
+    @Id
+    @Column(name = "student_name")
+    private String name;
+    @Column(name = "subscription_date")
+    private LocalDateTime subscriptionDate;
+
+    @EqualsAndHashCode
+    @ToString
+    @AllArgsConstructor
+    public class PurchaseListKey implements Serializable {
+
+        static final long serialVersionUID = 1L;
+        @Getter
+        @Setter
+        private String courseName;
+
+        @Getter
+        @Setter
+        private String name;
+    }
+}
