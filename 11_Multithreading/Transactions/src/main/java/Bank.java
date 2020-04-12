dc98e889ce6cc645a6672cfb3f7fd79788a74322
Super Oleg <oleg_in@bk.ru>
2020-04-12 22:50:18 +0300

    SecurityService class displaced to Bank class

--- 11_Multithreading/Transactions/src/main/java/Bank.java
+++ 11_Multithreading/Transactions/src/main/java/Bank.java
@@ -2,8 +2,12 @@ import lombok.extern.log4j.Log4j2;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Log4j2
 public class Bank {
@@ -21,10 +25,6 @@ public class Bank {
             throws InterruptedException {
         Thread.sleep(1000);
         return random.nextBoolean();
-    }
-
-    public boolean isFraudPub(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException {
-        return isFraud(fromAccountNum, toAccountNum, amount);
     }
 
     public void transfer(Integer fromAccountNum, Integer toAccountNum, double amountIn) {
@@ -82,6 +82,64 @@ public class Bank {
 
     private enum TransactionStatus {
         BLOCKED, COMMITED, NEED_CHECK
+    }
+
+    private static class SecurityService implements Runnable {
+
+        final Deque<Transaction> TRANSACTIONS = new ArrayDeque<>();
+        private final Bank BANK;
+
+        SecurityService(Bank bank) {
+            BANK = bank;
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.submit(this);
+        }
+
+        @Override
+        public void run() {
+            Transaction transaction;
+            while (true) {
+                synchronized (this) {
+                    while ((transaction = TRANSACTIONS.poll()) == null) {
+                        try {
+                            this.wait();
+                        } catch (InterruptedException e) {
+                            log.error(e);
+                        }
+                    }
+                }
+                Account from = transaction.getFrom();
+                Account to = transaction.getTo();
+                BigDecimal amount = transaction.getAmount();
+                Account.SyncAccs syncAccs = new Account.SyncAccs(from, to);
+                try {
+                    if (BANK.isFraud(String.valueOf(from.getAcc_number()),
+                            String.valueOf(to.getAcc_number()),
+                            amount.longValue())) {
+                        log.info("The operation did not pass the security check. Accounts are blocked");
+                        continue;
+                    }
+                } catch (InterruptedException e) {
+                    synchronized (syncAccs.syncAcc1) {
+                        synchronized (syncAccs.syncAcc2) {
+                            to.setBlocked(false);
+                            from.setBlocked(false);
+                            log.error(e);
+                        }
+                    }
+                }
+                synchronized (syncAccs.syncAcc1) {
+                    synchronized (syncAccs.syncAcc2) {
+                        from.setBlocked(false);
+                        to.setBlocked(false);
+                        from.setMoney(from.getMoney().subtract(amount));
+                        to.setMoney(to.getMoney().add(amount));
+                    }
+                }
+                log.info(String.format("Money sent from %s to %s: %.2f у.е.",
+                        from.getAcc_number(), to.getAcc_number(), amount.doubleValue()));
+            }
+        }
     }
 }
 
