e9c91639a2991a0ee4071b420a8a9d6df012c3f8
Super Oleg <oleg_in@bk.ru>
2020-05-26 23:49:47 +0300

    hotfix added

--- 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/config/WebSecurityConfig.java
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/config/WebSecurityConfig.java
@@ -13,10 +13,23 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
