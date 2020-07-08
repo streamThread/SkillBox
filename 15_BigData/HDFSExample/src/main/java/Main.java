da6d5a836b8e082c993174bc459ebdba62f1fcfa
Super Oleg <oleg_in@bk.ru>
2020-07-08 23:17:59 +0300

    [strmthrd] reformat code after review

--- 15_BigData/HDFSExample/src/main/java/Main.java
+++ 15_BigData/HDFSExample/src/main/java/Main.java
@@ -1,19 +1,20 @@
 public class Main {
 
-    private static final String ROOT_PATH = "hdfs://f8502fd9fbed:8020";
+  private static final String ROOT_PATH = "hdfs://f8502fd9fbed:8020";
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        FileAccess fileAccess = new FileAccess(ROOT_PATH);
+    FileAccess fileAccess = new FileAccess(ROOT_PATH);
 
-        fileAccess.create("/test/");
-        fileAccess.create("/test/test.txt");
-        fileAccess.append("/test/test.txt", "Hello Hadoop from JavaCode");
-        System.out.println(fileAccess.read("/test/test.txt"));
-        System.out.println("Directory \"test/\" is exists: " + fileAccess.isDirectory("/test/"));
-        System.out.println("List of paths in \"test/\": " + String.join(", ", fileAccess.list("/test/")));
-        System.out.println("Deleting \"test/\"");
-        fileAccess.delete("/test/");
-        System.out.println("Directory \"test/\" is exists: " + fileAccess.isDirectory("/test/"));
-    }
+    fileAccess.create("/test/");
+    fileAccess.create("/test/test.txt");
+    fileAccess.append("/test/test.txt", "Hello Hadoop from JavaCode");
+    System.out.println(fileAccess.read("/test/test.txt"));
+    System.out.println("Directory \"test/\" is exists: " + fileAccess.isDirectory("/test/"));
+    System.out.println("List of paths in \"test/\": " +
+        String.join(", ", fileAccess.list("/test/")));
+    System.out.println("Deleting \"test/\"");
+    fileAccess.delete("/test/");
+    System.out.println("Directory \"test/\" is exists: " + fileAccess.isDirectory("/test/"));
+  }
 }
